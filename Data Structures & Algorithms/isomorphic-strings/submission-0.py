ce187c5c1fe540d3181f47c0e74c5b4e82e541d3
manikandanramkumar <47510405+manikandanramkumar@users.noreply.github.com>
2026-05-15 13:22:38 -0400

    Add: isomorphic-strings - submission-0

---
+++ Data Structures & Algorithms/isomorphic-strings/submission-0.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s) == len(t):
+            sd = defaultdict(list)
+            td = defaultdict(list)
+            for i in range(len(s)):
+                sd[s[i]].append(i)
+                td[t[i]].append(i)
+            s_result = {}
+            t_result = {}
+            i,j =0,0
+            for (k,v) in (sd.items()):
+                s_result[i] = v
+                i +=1
+            for k,v in td.items():
+                t_result[j] =v
+                j +=1
+                
+            
+                
+                
+            print(sd)
+            print(s_result)
+            print(td)
+            print(t_result)
+            if s_result == t_result:
+                return True
+            else :
+                return False
