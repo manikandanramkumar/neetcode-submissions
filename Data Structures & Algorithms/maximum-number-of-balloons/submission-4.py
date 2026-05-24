c0aa8405d4e61671b65dc90e10ac83fe41ba217c
manikandanramkumar <47510405+manikandanramkumar@users.noreply.github.com>
2026-05-24 12:28:50 -0400

    Add: maximum-number-of-balloons - submission-4

---
+++ Data Structures & Algorithms/maximum-number-of-balloons/submission-4.py
@@ -0,0 +1,16 @@
+class Solution:
+    
+    def maxNumberOfBalloons(self, text: str) -> int:
+        c = Counter(text)
+        lt1= [c['b'],c['a'],c['n']]
+        lt2 = [c['l'],c['o']]
+       
+        print(min(lt1))
+        print(min(lt2))
+        print(min(lt2)//2)
+        if  min(lt1) >= min(lt2)//2  :
+            return min (lt2)//2
+        else :
+            return 0
+      
+        
